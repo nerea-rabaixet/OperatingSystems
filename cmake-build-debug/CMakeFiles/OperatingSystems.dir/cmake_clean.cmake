ecc1da957f1ddbbd7572ae90d1095e787d876b07
nerea-rabaixet <72511251+nerea-rabaixet@users.noreply.github.com>
2021-01-08 11:14:29 +0100

    testCode

--- cmake-build-debug/CMakeFiles/OperatingSystems.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/OperatingSystems.dir/cmake_clean.cmake
@@ -1,5 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OperatingSystems.dir/test.c.o"
+  "OperatingSystems.exe"
+  "OperatingSystems.pdb"
+  "libOperatingSystems.dll.a"
+)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
+foreach(lang C)
   include(CMakeFiles/OperatingSystems.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
